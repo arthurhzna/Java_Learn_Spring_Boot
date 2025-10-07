0b788cce4061ffc5e5be391d344d6b1a5ed5efc6
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-10-08 05:00:03 +0700

    learn mavem

---
+++ 2-mavem/learn-mavem/src/main/java/com/learnspringboot/learn_mavem/LearnMavemApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.learn_mavem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnMavemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnMavemApplication.class, args);
+	}
+
+}
---
+++ 2-mavem/learn-mavem/src/test/java/com/learnspringboot/learn_mavem/LearnMavemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.learn_mavem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMavemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
