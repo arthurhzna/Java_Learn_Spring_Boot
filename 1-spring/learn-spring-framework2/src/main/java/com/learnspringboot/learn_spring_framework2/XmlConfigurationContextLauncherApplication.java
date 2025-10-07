bf25258ef297dc1c25d4522c89773fd49afe7a57
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-10-08 03:48:47 +0700

    learn java spring xml configuration

---
+++ 1-spring/learn-spring-framework2/src/main/java/com/learnspringboot/learn_spring_framework2/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,56 @@
+package com.learnspringboot.learn_spring_framework2;
+
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+// interface GamingConsole {
+// 	void up();
+// 	void down();
+// 	void left();
+// 	void right();
+// }
+
+// @Component
+// class GameRunner {
+	
+// 	private GamingConsole game;
+	
+// 	public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game) {
+// 		this.game = game;
+// 	}
+
+// 	public void run() {
+		
+// 		System.out.println("Running game: " + game);
+// 		game.up();
+// 		game.down();
+// 		game.left();
+// 		game.right();
+		
+// 	}
+
+// }
+
+public class XmlConfigurationContextLauncherApplication {
+	
+	public static void main(String[] args) {
+
+		try (var context = 
+				new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+			
+			// Arrays.stream(context.getBeanDefinitionNames())
+			// 	.forEach(System.out::println);
+			
+			System.out.println(context.getBean("name"));
+			
+			System.out.println(context.getBean("age"));
+			
+			// context.getBean(GameRunner.class).run();
+
+		}
+	}
+}
