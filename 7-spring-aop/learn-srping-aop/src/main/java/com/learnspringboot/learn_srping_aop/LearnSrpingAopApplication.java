9c7a8c89b79ba403203adc4f587c15382d7e5e7e
Arthur Hozanna <90093341+arthurhzna@users.noreply.github.com>
2025-10-11 14:45:38 +0700

    learn aop

---
+++ 7-spring-aop/learn-srping-aop/src/main/java/com/learnspringboot/learn_srping_aop/LearnSrpingAopApplication.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.learn_srping_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSrpingAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSrpingAopApplication.class, args);
+	}
+
+}
---
+++ 7-spring-aop/learn-srping-aop/src/test/java/com/learnspringboot/learn_srping_aop/LearnSrpingAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnspringboot.learn_srping_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSrpingAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
